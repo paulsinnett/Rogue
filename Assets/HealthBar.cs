f0fb2e48a0ac874cf5bf67cf6ff0e593c9ca0d98
Paul Sinnett <paul.sinnett@gmail.com>
2015-02-17 16:04:53 +0000

    Initial commit

---
+++ Assets/HealthBar.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HealthBar : MonoBehaviour {
+
+	Slider slider;
+
+	void Start () {
+	
+		slider = GetComponent<Slider> ();
+	}
+
+	void SetValue(float value) {
+
+		slider.value = value;
+	}
+}
